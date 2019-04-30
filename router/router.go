ea27747c1ee7ccad7b32cf7f8b5918abd46e4a96
Luca Cotti <lucacotti@outlook.com>
2019-05-01 00:54:48 +0200

    Add routes for show to router

--- router/router.go
+++ router/router.go
@@ -3,19 +3,27 @@ package router
 import (
 	"net/http"
 
-	"github.com/LucaCtt/thelist/store"
+	"github.com/LucaCtt/thelist/data"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 )
 
-func New(store store.Store) http.Handler {
+func New(store data.Store) http.Handler {
 	router := chi.NewRouter()
-
-	router.Mount("/show", showRouter(store))
 
 	router.Use(middleware.StripSlashes)
 	router.Use(middleware.Logger)
 	router.Use(middleware.Recoverer)
+	router.Use(contentTypeMiddleware)
+
+	router.Mount("/show", showRouter(store))
 
 	return router
+}
+
+func contentTypeMiddleware(next http.Handler) http.Handler {
+  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+    next.ServeHTTP(w, r)
+  })
 }
--- router/show.go
+++ router/show.go
@@ -5,11 +5,11 @@ import (
 	"net/http"
 	"strconv"
 
-	"github.com/LucaCtt/thelist/store"
+	"github.com/LucaCtt/thelist/data"
 	"github.com/go-chi/chi"
 )
 
-func showRouter(store store.Store) http.Handler {
+func showRouter(store data.Store) http.Handler {
 	router := chi.NewRouter()
 
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
@@ -39,5 +39,40 @@ func showRouter(store store.Store) http.Handler {
 		json.NewEncoder(w).Encode(show)
 	})
 
+	router.Post("/", func(w http.ResponseWriter, r *http.Request) {
+		var show data.Show
+		err := json.NewDecoder(r.Body).Decode(&show)
+
+		if err != nil {
+			w.WriteHeader(http.StatusNotAcceptable)
+			return
+		}
+
+		err = store.CreateShow(&show)
+
+		if err != nil {
+			panic(err)
+		}
+
+		w.WriteHeader(http.StatusNoContent)
+	})
+
+	router.Delete("/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id, err := strconv.ParseUint(chi.URLParam(r, "id"), 10, 64)
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+		}
+
+		err = store.DeleteShow(uint(id))
+		if err != nil {
+			if store.IsRecordNotFoundError(err) {
+				w.WriteHeader(http.StatusNotFound)
+				return
+			}
+			panic(err)
+		}
+
+		w.WriteHeader(http.StatusNoContent)
+	})
 	return router
 }
