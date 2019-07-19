fe545b8f86ff247ba9a2a8be770c3044aeecc6f6
Luca Cotti <lucacotti@outlook.com>
2019-07-19 23:45:26 +0200

    Add constants package

---
+++ constants/constants.go
@@ -0,0 +1,8 @@
+package constants
+
+const (
+	apiBaseURL        = "https://api.themoviedb.org/3"
+	apiSearchEndpoint = "/search/multi"
+	apiKeyParam       = "api_key"
+	apiQueryParam     = "query"
+)
